d27ecd57e0720e121236d47a9b0fa7da30ac29e7
adityap <i91987@SLCITS-L2232.hcinsight.net>
2015-11-02 08:10:28 -0700

    intial using swashbuckle custom UI

---
+++ WebApidocGenSwagbuckleCustom/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApidocGenSwagbuckleCustom.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        /// <summary>
+        /// This is Get Doc
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// This is Get doc by Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        /// <summary>
+        /// This is Post Doc
+        /// </summary>
+        /// <param name="value"></param>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// This is Put doc
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        /// <summary>
+        /// This is Delete Doc
+        /// </summary>
+        /// <param name="id"></param>
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApidocGenSwagbuckleCustom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApidocGenSwagbuckleCustom.WebApiApplication" Language="C#" %>
